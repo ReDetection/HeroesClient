ac6c801eba29788a1ec60ed190410edbb613380e
Serg Buglakov <redetection@gmail.com>
2016-05-03 12:18:51 +0600

    scale to fit in view frame

--- heroesrfbclient/RFBRectView.swift
+++ heroesrfbclient/RFBRectView.swift
@@ -22,6 +22,7 @@ class RFBRectView: UIView {
 
             if frameRect.height > 0 && frameRect.width > 0 {
                 CGContextTranslateCTM(ctx, -frameRect.origin.x, -frameRect.origin.y)
+                CGContextScaleCTM(ctx, self.bounds.size.width / frameRect.size.width, self.bounds.size.height / frameRect.size.height)
                 framebuffer.drawRect(frameRect, context: ctx)
                 
             } else {
@@ -35,10 +36,12 @@ class RFBRectView: UIView {
     func setNeedsDisplayInOuterRect(outerRect: CGRect) {
         if frameRect.height > 0 && frameRect.width > 0 {
             var drawRect = CGRectZero
-            drawRect.origin.x = max(outerRect.origin.x - frameRect.origin.x, 0)
-            drawRect.origin.y = max(outerRect.origin.y - frameRect.origin.y, 0)
-            drawRect.size.height = min(outerRect.size.height, frameRect.size.height - drawRect.origin.y )
-            drawRect.size.width = min(outerRect.size.width, frameRect.size.width - drawRect.origin.x)
+            let scaleX = self.bounds.size.width / frameRect.size.width
+            let scaleY = self.bounds.size.height / frameRect.size.height
+            drawRect.origin.x = max(outerRect.origin.x - frameRect.origin.x, 0) * scaleX
+            drawRect.origin.y = max(outerRect.origin.y - frameRect.origin.y, 0) * scaleY
+            drawRect.size.height = min(outerRect.size.height, frameRect.size.height - drawRect.origin.y ) * scaleY
+            drawRect.size.width = min(outerRect.size.width, frameRect.size.width - drawRect.origin.x) * scaleX
             if drawRect.size.width > 0 && drawRect.size.height > 0 {
                 self.setNeedsDisplayInRect(drawRect)
             }
